0c603b9390ae02f888f6581abe0b1947b637f61a
Sumit-webuildtech <sumit@webuildtech.in>
2026-06-16 13:13:40 +0530

    feat: add Google Tag Manager tracking code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
-
 export const metadata: Metadata = {
   title: "OneOrange",
   description: "A marketing company built to scale brands.",
@@ -39,7 +39,28 @@ export default function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body className="antialiased">{children}</body>
+      <body className="antialiased">
+        {/* Google Tag Manager (noscript) */}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T68SGLJV" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
+        {/* End Google Tag Manager (noscript) */}
+        
+        {children}
+
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive" dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-T68SGLJV');
+          `
+        }} />
+      </body>
     </html>
   );
 }
